7a4b78dc778f92ab8c4d200040daf9c65fdf95bc
Rappat-Temp <adityasrivastavamicroppt@gmail.com>
2026-04-24 07:28:05 +0530

    Saving files before refreshing line endings

--- CodeCollab_Frontend/src/App.jsx
+++ CodeCollab_Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Landing from './pages/landing.jsx'
+import Login from './pages/login.jsx'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   )
---
+++ CodeCollab_Frontend/src/components/login/verifyLink.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const verifyLink = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default verifyLink
